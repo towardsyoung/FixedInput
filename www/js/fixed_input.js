754c51093e06a6154b425efd9a61bdd42c804c71
陈朝阳 <chenchaoyang@iyuesao.com>
2015-08-24 16:50:41 +0800

    plugin 配置

--- www/js/fixed_input.js
+++ www/js/fixed_input.js
@@ -1,10 +1,22 @@
     var exec = require('cordova/exec');
     var fixedInput = {
-        show:function(sendCallback, defaultVal) {
-            exec(sendCallback, null, "FixedInput", "show", [defaultVal]);
+	show:function(sendCallback, defaultVal, btnText) {
+            if (!defaultVal) {
+               defaultVal = "";
+            }
+            if (!btnText) {
+               btnText = "回复";
+            }
+            exec(sendCallback, null, "FixedInput", "show", [defaultVal, btnText]);
         },
-        showAndFocus:function(sendCallback, defaultVal) {
-            exec(sendCallback, null, "FixedInput", "showAndFocus", [defaultVal]);
+        showAndFocus:function(sendCallback, defaultVal, btnText) {
+            if (!defaultVal) {
+               defaultVal = "";
+            }
+            if (!btnText) {
+               btnText = "回复";
+            }
+            exec(sendCallback, null, "FixedInput", "showAndFocus", [defaultVal, btnText]);
         },
         hide:function() {
             exec(null, null, "FixedInput", "hide", []);
